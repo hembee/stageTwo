c06648d196a569dae09381bf8e0b00c923768e06
hembee <hembee999@gmail.com>
2023-09-12 23:49:54 +0100

    mongo uri fixed

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const joi = require("joi");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 4000;
 const UserRouter = require("./src/routes/person.route.js");
 dotenv.config();
 
+const PORT = process.env.PORT;
+
 //DATABASE CONNECTION
 mongoose
-  .connect(
-    // process.env.MONGO_URI ||
-    "mongodb+srv://cash2go:cash2go@blax.sa6yubn.mongodb.net/stageTwo?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Database connection establised"))
   .catch((e) => console.log("Mongo connection error: ", e.message));
 
