f1682ce5051de6bf6d2a4c1fb74070ecdea93ad5
hembee <hembee999@gmail.com>
2023-09-13 08:57:38 +0100

    routes

--- src/routes/person.route.js
+++ src/routes/person.route.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const personController = require("../controllers/person.controller");
 
-
 router.get("/", (req, res) => {
-    res.send("IT WORKS!!!!!")
-})
+  res.send("IT WORKS!!!!!");
+});
 
+router.post("/", personController.createPersonController);
+router.get("/user_id", personController.readUserController);
+router.put("/user_id", personController.updateUserController);
+router.delete("/user_id", personController.deleteUserController);
 
-
-
-module.exports= router
+module.exports = router;
